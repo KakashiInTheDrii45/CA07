1b513eac6fec4009b6757adc223bb8b16d75cc24
KakashiInTheDrii45 <116908297+KakashiInTheDrii45@users.noreply.github.com>
2026-03-11 23:56:36 +0200

    Completed the fillArray() and started with the displayArray method

--- testShop.java
+++ testShop.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
 public class testShop{
 
+   
+    public static fillArray(Shop arrShop[]){
+        Scanner scn = new Scanner(System.in);
+        String shopName = " ";
+        int i=0;
+        while(shopName.toLowerCase() != "x"){
+        System.out.print("Enter the name of shop (X to quit) : " );
+        shopName = scn.nextLine();
+        Shop tempShop = new Shop(shopName); 
+        arrShop[i] = tempShop;
+        i++;
+
+        }
+    }
+
+    public static void DisplayArray(){
+        System.out.println();
+        for(int i=0;i<Shop.getNumberOfShops(); i++){
+
+            Shop.toString();
+
+        }
+        
+    }
 
 
-    
+    public static void  main(String[] args){
+        
+    Shop[] arrShops = new Shop[10];
+    fillArray();
+
+    }
 }
-    public void DisplayArray(){}
+    
